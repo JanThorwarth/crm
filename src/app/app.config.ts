9e1c4426e50eb0008de433e86cd21d8d9fef8258
Jan <thorwarth.jan@web.de>
2025-02-18 14:46:49 +0100

    added signup component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({ projectId: "crm-app-beba5", appId: "1:953915033068:web:e1598720250c7ce743acc9", storageBucket: "crm-app-beba5.firebasestorage.app", apiKey: "AIzaSyD42Gm3vBDd0eyyp1sHTDt8HA1R4oOZLd4", authDomain: "crm-app-beba5.firebaseapp.com", messagingSenderId: "953915033068" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())]
 };
